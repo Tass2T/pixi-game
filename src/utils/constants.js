7057b9aedca6a959b961795afab5c87c3ac274da
tass2t <brunog90@gmail.com>
2023-02-02 17:00:05 +0100

    dynamic scene size

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const WIDTH = 1200
-export const HEIGHT = 720
+export const WIDTH = Math.min(1280, window.innerWidth);
+export const HEIGHT = Math.min(720, window.innerHeight);
 export const DEFAULT_FRAME = 1;
 export const ANIMATION_SPEED = 0.09;
 export const CHARACTER_SPEED = 5;
