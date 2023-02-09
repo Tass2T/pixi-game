5e0bfb6c24393022e5c3693759e5f528799f637e
tass2t <brunog90@gmail.com>
2023-02-09 16:41:46 +0100

    better villain positioning and proper informations on the spritesheet

--- src/Game/Characters/Villain.js
+++ src/Game/Characters/Villain.js
@@ -8,12 +8,10 @@ export default class Villain extends Character {
     super(scene);
     this.spriteManager = new SpriteManager();
     this.prepareSprite(originDirection);
-    this.setPosition(originDirection);
     this.destination = {
       x: 0,
       y: 0,
     };
-    this.scene.addChild(this.sprite);
   }
 
   prepareSprite = (originDirection) => {
@@ -22,24 +20,26 @@ export default class Villain extends Character {
       this.sheet.animations[originDirection.toLowerCase()]
     );
     this.sprite.anchor.set(0.5);
+    this.setPosition(originDirection);
+    this.scene.addChild(this.sprite);
   };
 
   setPosition = (originDirection) => {
     switch (originDirection) {
       case "DOWN":
         this.sprite.x = Math.floor(Math.random() * WIDTH);
-        this.sprite.y = 0;
+        this.sprite.y = 40;
         break;
       case "UP":
         this.sprite.x = Math.floor(Math.random() * WIDTH);
-        this.sprite.y = HEIGHT - 85;
+        this.sprite.y = HEIGHT - 40;
         break;
       case "RIGHT":
-        this.sprite.x = 0;
+        this.sprite.x = 40;
         this.sprite.y = Math.floor(Math.random() * HEIGHT);
         break;
       case "LEFT":
-        this.sprite.x = WIDTH - 100;
+        this.sprite.x = WIDTH - 40;
         this.sprite.y = Math.floor(Math.random() * HEIGHT);
         break;
     }
