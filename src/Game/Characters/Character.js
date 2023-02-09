c7ae73d703deb5246921aa337159ed412a117249
tass2t <brunog90@gmail.com>
2023-02-09 14:32:36 +0100

    removed useless Character trait

--- src/Game/Characters/Character.js
+++ src/Game/Characters/Character.js
@@ -1,10 +1,6 @@
 export default class Character {
   constructor(scene) {
     this.scene = scene;
-    this.position = {
-      x: 0,
-      y: 0,
-    };
     this.destination = {
       x: 0,
       y: 0,
