f21853126e6b90f83e5fa8180c930e9c0bf1c385
tass2t <brunog90@gmail.com>
2023-02-16 14:52:46 +0100

    lint

--- src/Pixi.js
+++ src/Pixi.js
@@ -8,19 +8,19 @@ import SpriteManager from "./Game/Various/SpriteManager";
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
-let instance = null
+let instance = null;
 
 export default class Pixi {
   constructor() {
-    if (instance) return instance
-    instance = this
+    if (instance) return instance;
+    instance = this;
     const gameArea = document.querySelector("#app");
     this.app = new PIXI.Application({
       width: WIDTH,
       height: HEIGHT,
       backgroundColor: 0x000000,
     });
-    this.spriteManager = new SpriteManager()
+    this.spriteManager = new SpriteManager();
 
     // use gsap ticker instead
     this.app.ticker.stop();
@@ -29,26 +29,25 @@ export default class Pixi {
       this.app.ticker.update();
     });
     this.scene = this.app.stage;
-    this.scene.interactive = true
+    this.scene.interactive = true;
     gameArea.appendChild(this.app.view);
 
     this.init();
-      let elapsed = 0.0;
-      this.app.ticker.maxFPS = 60;
-      this.app.ticker.add((delta) => {
-        elapsed += delta;
-        this.update();
-      });
-    
+    let elapsed = 0.0;
+    this.app.ticker.maxFPS = 60;
+    this.app.ticker.add((delta) => {
+      elapsed += delta;
+      this.update();
+    });
   }
 
   init = async () => {
-    await this.spriteManager.loadTextures()
+    await this.spriteManager.loadTextures();
     this.game = new Game(this.scene);
-  }
+  };
 
   update() {
-    if (this.game)this.game.update();
+    if (this.game) this.game.update();
   }
 
   dispose() {}
