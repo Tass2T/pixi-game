cf347eaf16004e6d63da6cc3fc1c797ae94c020a
tass2t <brunog90@gmail.com>
2023-02-18 21:00:10 +0100

    MERGE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const WIDTH = Math.min(1280, window.innerWidth);
-export const HEIGHT = Math.min(1280, window.innerHeight);
+export const WIDTH = 1350;
+export const HEIGHT = 1350
 export const DEFAULT_FRAME = 1;
 export const ANIMATION_SPEED = 0.09;
 export const CHARACTER_SPEED = 5;
