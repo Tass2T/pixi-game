d1fd4369e6011ce8549dac3a94e7c1f1a3e238ab
tass2t <brunog90@gmail.com>
2023-02-21 11:28:13 +0100

    prepared state management

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,18 +1,51 @@
 import { Sprite } from "pixi.js";
 import Player from "./Characters/Player";
-import { BACKGROUND_SPRITE_SIZE, BACKGROUND_SPRITE_NUMBER } from "../utils/constants";
+import {
+  BACKGROUND_SPRITE_SIZE,
+  BACKGROUND_SPRITE_NUMBER,
+} from "../utils/constants";
 import SpriteManager from "./Various/SpriteManager";
 import VillainManager from "./Characters/VillainManager";
 import { CollisionManager } from "./Various/CollisionManager";
 import Pixi from "../Pixi";
 
 const level = [
-  [Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER)],
-  [Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER)],
-  [Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER)],
-  [Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER)],
-  [Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER)]
-]
+  [
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+  ],
+  [
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+  ],
+  [
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+  ],
+  [
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+  ],
+  [
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+    Math.floor(Math.random() * BACKGROUND_SPRITE_NUMBER),
+  ],
+];
 
 export default class Game {
   constructor() {
@@ -22,35 +55,42 @@ export default class Game {
     this.prepareBackground();
     this.player = new Player(this.scene);
     this.villainManager = new VillainManager();
-    this.collisionManager = new CollisionManager(this.player, this.villainManager)
+    this.collisionManager = new CollisionManager(
+      this.player,
+      this.villainManager
+    );
+    this.state = this.game;
     this.scene.on("click", this.player.shoot);
   }
 
   async prepareBackground() {
     this.backgroundTextures =
       this.spriteManager.getTexture("backgroundTexture");
-      
-    let x = 0  
-    let y = 0
 
-    level.forEach(line => {
-      x = 0
-      line.forEach(element => {
-        const sprite = Sprite.from(this.backgroundTextures.textures[element])
-        sprite.x = x
-        sprite.y = y
-        this.scene.addChild(sprite)
-        x+= BACKGROUND_SPRITE_SIZE
-      })
-      y += BACKGROUND_SPRITE_SIZE
+    let x = 0;
+    let y = 0;
+
+    level.forEach((line) => {
+      x = 0;
+      line.forEach((element) => {
+        const sprite = Sprite.from(this.backgroundTextures.textures[element]);
+        sprite.x = x;
+        sprite.y = y;
+        this.scene.addChild(sprite);
+        x += BACKGROUND_SPRITE_SIZE;
+      });
+      y += BACKGROUND_SPRITE_SIZE;
     });
-
   }
-  update() {
+  update = () => {
+    this.state();
+  };
+
+  game = () => {
     this.player.update();
     this.villainManager.update();
-    this.collisionManager.update()
-  }
+    this.collisionManager.update();
+  };
 
   dispose() {}
 }
