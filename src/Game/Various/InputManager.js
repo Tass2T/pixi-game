a09aea2bf3b499c3852f4d5a585fe790fb2cfcad
tass2t <brunog90@gmail.com>
2023-02-23 00:25:10 +0100

    added basic stuff on input manager

--- src/Game/Various/InputManager.js
+++ src/Game/Various/InputManager.js
@@ -1,3 +1,21 @@
 export default class InputManager {
-  constructor() {}
+  constructor() {
+    this.keys = {};
+    window.addEventListener("keydown", this.manageInput);
+    window.addEventListener("keyup", this.manageInput);
+  }
+
+  manageInput = (e) => {
+    if (e.repeat) return;
+    switch (e.type) {
+      case "keydown":
+        this.keys[e.keyCode] = true;
+        break;
+      case "keyup":
+        this.keys[e.keyCode] = false;
+        break;
+      default:
+        break;
+    }
+  };
 }
