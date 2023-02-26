0faff2bc2a56d2a63073ff84cf7a919db99a18b2
tass2t <brunog90@gmail.com>
2023-02-27 00:02:36 +0100

    clean import

--- src/Game/Items/Bullets.js
+++ src/Game/Items/Bullets.js
@@ -1,7 +1,6 @@
 import { Rectangle, Sprite } from "pixi.js";
 import gsap from "gsap";
 import SpriteManager from "../Managers/SpriteManager";
-import Player from "../Characters/Player";
 import { BULLET_SPEED, HEIGHT, WIDTH } from "../../utils/constants";
 
 export class Bullet {
