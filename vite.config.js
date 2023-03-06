587e8b09b7f4e358c93f4c1fdbbcb6525f93ecb6
tass2t <brunog90@gmail.com>
2023-03-06 13:50:01 +0100

    added config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+  root: "./",
+  publicDir: "./src/assets/",
+  base: "./",
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+};
