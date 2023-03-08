17440a89deb3aa2a0b4a350bace3308fb77e2711
tass2t <brunog90@gmail.com>
2023-03-08 18:54:12 +0100

    change width and height

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const WIDTH = 1100;
-export const HEIGHT = 740;
+export const WIDTH = 1200;
+export const HEIGHT = 840;
 export const DEFAULT_FRAME = 1;
 export const ANIMATION_SPEED = 0.09;
 export const CHARACTER_SPEED = 5;
